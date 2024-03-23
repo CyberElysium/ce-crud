7af4c5acabf45237e64f8700a27d2e1b60f0df1b
klsameera <sameera@cyberelysium.com>
2024-03-23 17:58:34 +0530

    changes

--- src/Console/InstallCECrudCommand.php
+++ src/Console/InstallCECrudCommand.php
@@ -30,6 +30,12 @@ class InstallCECrudCommand extends Command
         $composerJson['autoload']['psr-4']['domain\\'] = 'domain/';
         file_put_contents(base_path('composer.json'), json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
+        // Create the HomeController
+        $this->createHomeController();
+
+        // Add the home route to web.php
+        $this->addHomeRoute();
+
         // Create views structure
         $this->createViewsStructureWithStubs([
             'components' => [
@@ -44,6 +50,10 @@ class InstallCECrudCommand extends Command
             'layouts' => [
                 'main.blade.php' => 'stubs/resources/layouts/main.stub',
             ],
+            'pages/home' => [
+                'index.blade.php' => 'stubs/resources/pages/home/index.stub',
+            ],
+
         ]);
 
         $this->info('CE-CRUD installed successfully. Please run "composer dump-autoload" to refresh autoload.');
@@ -80,5 +90,49 @@ class InstallCECrudCommand extends Command
         File::put($componentPath, $content);
 
         $this->info("Main layout created successfully.");
+    }
+
+    protected function createHomeController()
+    {
+        $controllerPath = app_path('Http/Controllers/HomeController.php');
+        $controllerContent = "<?php\n\nnamespace App\Http\Controllers;\n\nuse Illuminate\Http\Request;\n\nclass HomeController extends Controller\n{\n    public function index()\n    {\n        return view('pages.home.index');\n    }\n}\n";
+
+        if (!File::exists($controllerPath)) {
+            File::put($controllerPath, $controllerContent);
+            $this->info("HomeController created successfully.");
+        } else {
+            $this->info("HomeController already exists.");
+        }
+    }
+
+    protected function addHomeRoute()
+    {
+        $webRoutePath = base_path('routes/web.php');
+
+        // Read the existing web.php file content
+        $webRoutes = File::get($webRoutePath);
+
+        // Prepare the use statement and the route definition
+        $useStatement = "use App\Http\Controllers\HomeController;";
+        $homeRoute = "Route::get('/', [HomeController::class, 'index'])->name('home');\n";
+
+        // Check if the use statement is already in the file
+        if (!str_contains($webRoutes, $useStatement)) {
+            // If not, insert it after the PHP opening tag
+            $webRoutes = str_replace('<?php', "<?php\n\n" . $useStatement . "\n", $webRoutes);
+            $this->info("Import statement for HomeController added to web.php.");
+        }
+
+        // Check if the home route is already in the file
+        if (!str_contains($webRoutes, "Route::get('/', [HomeController::class, 'index'])")) {
+            // If not, append the home route to the web.php file
+            $webRoutes .= "\n" . $homeRoute;
+            $this->info("Home route added successfully to web.php.");
+        } else {
+            $this->info("Home route already exists in web.php.");
+        }
+
+        // Write the changes back to web.php
+        File::put($webRoutePath, $webRoutes);
     }
 }
