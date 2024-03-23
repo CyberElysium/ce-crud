9cae54cb9157542db12c58eb81ee97900a0894fb
klsameera <sameera@cyberelysium.com>
2024-03-23 19:56:00 +0530

    changes

--- src/Console/MakeCECrudCommand.php
+++ src/Console/MakeCECrudCommand.php
@@ -24,7 +24,13 @@ class MakeCECrudCommand extends Command
         $this->updateMigrationFile($name, $tableFields);
         $this->updateModelFile($name, $tableFields);
 
-        Artisan::call('make:controller', ['name' => "{$name}Controller"]);
+        // Artisan::call('make:controller', ['name' => "{$name}Controller"]);
+
+        $controllerStubPath = __DIR__ . '/../stubs/Controller.stub';
+        $this->createController($name, $controllerStubPath);
+
+        // Create the CRUD route file
+        $this->createCrudRouteFile($name);
 
         $serviceStubPath = __DIR__ . '/../stubs/Service.stub';
         $facadeStubPath = __DIR__ . '/../stubs/Facade.stub';
@@ -118,6 +124,70 @@ class MakeCECrudCommand extends Command
             $fillableString = "protected \$fillable = ['" . $fillableArray . "'];\n";
             $contents = preg_replace('/(class .* extends Model\s*{)/', '$1' . "\n    " . $fillableString, $contents);
             File::put($modelPath, $contents);
+        }
+    }
+
+    protected function createCrudRouteFile($name)
+    {
+        $routeDir = base_path("routes/CeCrud");
+        File::ensureDirectoryExists($routeDir);
+
+        $namePlural = Str::plural(Str::snake($name));
+
+        $routeFilePath = "{$routeDir}/{$name}.php";
+        $routeTemplate = <<<ROUTE
+            <?php
+
+            use Illuminate\Support\Facades\Route;
+            use App\Http\Controllers\\{$name}Controller;
+
+            Route::prefix('{$namePlural}')->group(function () {
+                Route::get('/', [{$name}Controller::class, 'index'])->name('{$namePlural}.index');
+                Route::get('/create', [{$name}Controller::class, 'create'])->name('{$namePlural}.create');
+                Route::post('/', [{$name}Controller::class, 'store'])->name('{$namePlural}.store');
+                Route::get('/{id}/edit', [{$name}Controller::class, 'edit'])->name('{$namePlural}.edit');
+                Route::put('/{id}', [{$name}Controller::class, 'update'])->name('{$namePlural}.update');
+                Route::delete('/{id}', [{$name}Controller::class, 'destroy'])->name('{$namePlural}.destroy');
+            });
+            ROUTE;
+
+        File::put($routeFilePath, $routeTemplate);
+        $this->info("{$name} route file created successfully.");
+
+        // Include the new route file in web.php
+        $webRoutePath = base_path('routes/web.php');
+        $routeRequireStatement = "\nrequire __DIR__.'/CeCrud/{$name}.php';";
+        if (strpos(File::get($webRoutePath), $routeRequireStatement) === false) {
+            File::append($webRoutePath, $routeRequireStatement);
+            $this->info("{$name} route file included in web.php successfully.");
+        }
+    }
+
+    protected function addRouteFileToWeb($name)
+    {
+        $webRoutesPath = base_path('routes/web.php');
+
+        $routeRequire = "require __DIR__.'/CeCrud/{$name}.php';\n";
+
+        if (!Str::contains(File::get($webRoutesPath), $routeRequire)) {
+            File::append($webRoutesPath, "\n" . $routeRequire);
+            $this->info("{$name} routes included in web.php successfully.");
+        }
+    }
+
+    protected function createController($name, $controllerStubPath)
+    {
+        $namePluralLower = Str::plural(Str::snake($name));
+        $controllerContent = file_get_contents($controllerStubPath);
+        $controllerContent = str_replace(['{{name}}', '{{nameLowerPlural}}'], [$name, $namePluralLower], $controllerContent);
+
+        $controllerPath = app_path("Http/Controllers/{$name}Controller.php");
+
+        if (!File::exists($controllerPath)) {
+            File::put($controllerPath, $controllerContent);
+            $this->info("{$name}Controller created successfully.");
+        } else {
+            $this->info("{$name}Controller already exists.");
         }
     }
 }
